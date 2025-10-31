723316026641362b9b8db4a3b9b7257b3fe5f84f
Opilane TTHK <opilane@tthk.ee>
2025-10-31 13:16:53 +0200

    For ja array näidised

---
+++ For/For/Program.cs
@@ -0,0 +1,29 @@
+﻿//using System.Runtime.ExceptionServices;
+
+//Console.WriteLine("For tsükkel");
+//int kogus = 666;
+
+//for (int i = 0; i < kogus; i++)
+//{
+//    Console.WriteLine("o((>w< ))o");
+//    Console.Beep();
+//}
+
+//string[] objectArray = new string[5]; //array, massiiv mitmest elemendist
+//int objectCount = 0; //loend kuyi palju kordi küsida
+//string temp = "no name"; //muutuja mis hetkel on "tühi"
+//while (objectCount < 5)
+//{
+//    Console.WriteLine("Sisesta järgmine lemmiklooma nimi");
+//    temp = Console.ReadLine();
+//    objectArray[objectCount] = temp;
+//    objectCount++;
+//}
+//Console.WriteLine("Oled sisestanud järgnevad loomanimed");
+//for (int i = 0; i < objectArray.Length; i++)
+//{
+//    Console.WriteLine($"{i + 1}. loomanimi ón: {objectArray[i]}");
+//}
+//                       0           1            2                 3         4
+string[] monsters = {"MrSketal","Frankenstein","Shigeru Myamoto","Kratt","Joodik Balti Jaamas" };
+Console.WriteLine(monsters[0]);
